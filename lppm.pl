9e7daa17830c3a38031c5415c055061c7e1554e5
Lucian Green <luciangreen@lucianacademy.com>
2021-07-02 14:31:31 +1000

    hierarchical dependencies

--- lppm.pl
+++ lppm.pl
@@ -205,14 +205,39 @@ lppm_install(User1,Repository1) :-
 	writeln1(Text2),read_string(user_input, "\n", "\r", _, Path1),
 	(working_directory(_,Path1)->true;(concat_list(["Warning: ",Path1," doesn't exist."],Text3),writeln1(Text3),fail))),
 	
+	%catch((true, call_with_time_limit(1,
+		find_all_dependencies(LPPM_registry_term1,[[User1,Repository1%%,Description,Dependencies1
+	]],Dependencies1,[],Dependencies1a)
+		,
+		%)),
+ %         time_limit_exceeded,
+  %        (concat_list(["Error: Cycle in lppm_registry.txt: ",Dependencies1],Note_a),writeln(Note_a),abort)),
+  
 	append([[User1,Repository1%%,Description,Dependencies1
-	]],Dependencies1,Dependencies2),
+	]],Dependencies1a,Dependencies2),
+
+  sort(Dependencies2,Dependencies2b),
 	
-	findall(_,(member(Dependency2,Dependencies2),Dependency2=[User3,Repository3],
+	
+	%trace,
+	%writeln(Dependencies2b),
+	findall(_,(member(Dependency2,Dependencies2b),Dependency2=[User3,Repository3],
 	concat_list(["git clone https://github.com/",User3,"/",Repository3,".git"],Command3),
  	catch(bash_command(Command3,_), _, (concat_list(["Warning."%%"Error: Can't clone ",User3,"/",Repository3," repository on GitHub."
  	],Text4),writeln1(Text4)%%,abort
  	))),_),!.
+ 	
+find_all_dependencies(_,_,[],A,A) :- !.
+find_all_dependencies(LPPM_registry_term1,Orig,Dependencies1,Dependencies8,Dependencies3) :-
+	Dependencies1=[[User1a,Repository2]|Dependencies9],
+	(member([User1a,Repository2,_Description,Dependencies7],LPPM_registry_term1)->true;abort),
+	append(Dependencies8,[[User1a,Repository2]],Dependencies10),
+
+		subtract(Dependencies7,Dependencies10,Dependencies11),
+find_all_dependencies(LPPM_registry_term1,Orig,Dependencies11,Dependencies10,Dependencies6),
+	find_all_dependencies(LPPM_registry_term1,Orig,Dependencies9,Dependencies6,Dependencies3).
+
+	
 	%%concat_list(["git pull https://github.com/",User3,"/",Repository3,".git master"],Command4),
  	%%catch(bash_command(Command4,_), _, (concat_list(["Error: Can't pull ",User3,"/",Repository3," repository on GitHub."],Text5),writeln1(Text5),abort))),_).
 	
